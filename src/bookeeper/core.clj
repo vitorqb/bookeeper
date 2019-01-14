9ea33b39c0c1770e88652eac999d0ed535961467
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-01-14 19:58:25 +0100

    Added getenv functionality

--- src/bookeeper/core.clj
+++ src/bookeeper/core.clj
@@ -3,10 +3,32 @@
   (:require [ragtime.jdbc :as jdbc]
             [ragtime.repl :as repl]))
 
+
+;;
+;; Settings
+;;
+(defn getenv [x] "Wraps System/getenv for testing" (System/getenv x))
+
+
+(defn getenv-or-error
+  "Get from env or throws runtime error"
+  [x]
+  (or (getenv x)
+      (throw (RuntimeException. (str "Env var " x " not found")))))
+
+
+(def db-subname (getenv-or-error "BOOKEEPER_DB_FILE"))
+
+
+;;
+;; Main
+;; 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
 
 
 ;; 
--- test/bookeeper/core_test.clj
+++ test/bookeeper/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [bookeeper.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-getenv-or-error
+  (testing "Returns when getenv returns"
+    (with-redefs [getenv (constantly "hola")]
+      (is (= (getenv-or-error "a")) "hola")))
+  (testing "Throws error if getenv returns nil"
+    (with-redefs [getenv (constantly nil)]
+      (is (thrown? RuntimeException (getenv-or-error "a"))))))
+
